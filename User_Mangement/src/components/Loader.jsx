1ca05b3e5202febf18580329e8ceb663b3ed2a87
Ajith163 <112544371+Ajith163@users.noreply.github.com>
2025-06-29 21:16:55 -0700

    commit changes

--- User_Mangement/src/components/Loader.jsx
+++
@@ -1,6 +0,0 @@
-import { Spin } from 'antd';
-export default () => (
-  <div style={{ textAlign: 'center', padding: '2rem' }}>
-    <Spin size="large" />
-  </div>
-);
--- User_Mangement/src/components/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
-
-export default function ProtectedRoute({ children }) {
-  const { token } = useSelector((s) => s.auth);
-  return token ? children : <Navigate to="/login" replace />;
-}
